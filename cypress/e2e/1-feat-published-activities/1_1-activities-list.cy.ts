0825b5ee6d0be2422bcc68dafbc926035dd585ba
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-13 12:33:23 +0200

    feat: 1_1 activities list

--- cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
+++ cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
@@ -11,12 +11,24 @@
 describe("Given the Activities list", () => {
   const PAGE_URL = "/";
   const API_URL = `${Cypress.env("apiUrl")}/activities?state=published`;
-
+  let publishedActivities: any[] = [];
+  let activitiesCounter = 0;
   context("when the page is loaded with the published activities", () => {
     beforeEach(() => {
       cy.visit(PAGE_URL);
+      cy.fixture("activities").then((fixture) => {
+        const activities = fixture as unknown as any[];
+        publishedActivities = activities.filter((a) => a.state == "published");
+        activitiesCounter = publishedActivities.length;
+        cy.intercept("GET", API_URL, {
+          body: publishedActivities,
+        });
+      });
     });
-    it("then should show the number of activities", () => {});
+    it.only("then should show the number of activities", () => {
+      cy.get("[name='items-count']").should("contain.text", activitiesCounter);
+      cy.get("[name='activity-item']").should("have.length", activitiesCounter);
+    });
     it("then should show activities name, price, and date", () => {});
     it("then should have a link to the activity page", () => {});
     it("then should list in a monospace font", () => {});
