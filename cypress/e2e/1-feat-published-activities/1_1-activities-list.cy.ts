ce6f28d214b60a3f2729d0b6375c5dd624cd8c14
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-15 09:34:01 +0200

    fix: 1 1

--- cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
+++ cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
@@ -1,11 +1,10 @@
 /**
- * Given the Activities list
- *   when the page is loaded with the published activities
+ * Given the Published Activities list
+ *   when the Home page is loaded
  *    then should show the number of activities
- *    then should show activities name, price, and date
  *    then should have a link to the activity page
  *    then should list in a monospace font
- *    then should only show published activities
+ *    then should show activities name, price, and date
  */
 
 describe("Given the Activities list", () => {
@@ -29,9 +28,8 @@ describe("Given the Activities list", () => {
       cy.get("[name='items-count']").should("contain.text", activitiesCounter);
       cy.get("[name='activity-item']").should("have.length", activitiesCounter);
     });
-    it("then should show activities name, price, and date", () => {});
     it("then should have a link to the activity page", () => {});
     it("then should list in a monospace font", () => {});
-    it("then should only show published activities", () => {});
+    it("then should show activities date", () => {});
   });
 });
