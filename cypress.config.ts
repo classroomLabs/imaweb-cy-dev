3e67aa76f6f05df5b260fc26dfe90356fda100e4
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-15 11:34:31 +0200

    chore: config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4200",
+    defaultCommandTimeout: 3000,
+    video: false,
+    trashAssetsBeforeRuns: true,
+    screenshotOnRunFailure: false,
+    viewportHeight: 768,
+    viewportWidth: 1024,
     env: {
       apiUrl: "http://localhost:3000",
       listName: "list-content",
