1bf3ed10f362d8f6f303e2ccefee51e89ef6e753
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-15 10:03:39 +0200

    feat: link

--- cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
+++ cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
@@ -12,6 +12,7 @@ describe("Given the Activities list", () => {
   const API_URL = `${Cypress.env("apiUrl")}/activities?state=published`;
   let publishedActivities: any[] = [];
   let activitiesCounter = 0;
+  let firstSlug = "";
   context("when the page is loaded with the published activities", () => {
     beforeEach(() => {
       cy.visit(PAGE_URL);
@@ -19,16 +20,24 @@ describe("Given the Activities list", () => {
         const activities = fixture as unknown as any[];
         publishedActivities = activities.filter((a) => a.state == "published");
         activitiesCounter = publishedActivities.length;
+        firstSlug = publishedActivities[0].slug;
         cy.intercept("GET", API_URL, {
           body: publishedActivities,
         });
       });
     });
-    it.only("then should show the number of activities", () => {
+    it("then should show the number of activities", () => {
       cy.get("[name='items-count']").should("contain.text", activitiesCounter);
       cy.get("[name='activity-item']").should("have.length", activitiesCounter);
     });
-    it("then should have a link to the activity page", () => {});
+    it("then should have a link to the activity page", () => {
+      cy.get('main[name="list-content"]')
+        .find("div")
+        .first()
+        .children('[name="title"]')
+        .children("a")
+        .should("have.attr", "href", `/activities/${firstSlug}`);
+    });
     it("then should list in a monospace font", () => {});
     it("then should show activities date", () => {});
   });
