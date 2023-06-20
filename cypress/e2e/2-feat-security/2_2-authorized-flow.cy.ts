9b483d7172e0038063077309ed4191785279bc37
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-20 11:15:08 +0200

    feat: register ui

--- cypress/e2e/2-feat-security/2_2-authorized-flow.cy.ts
+++ cypress/e2e/2-feat-security/2_2-authorized-flow.cy.ts
@@ -5,14 +5,27 @@
  *   should send the token to the server
  */
 describe("Given an already registered and logged user", () => {
-  const PAGE_URL = "http://localhost:4200/";
+  const PAGE_URL = "/";
   const API_URL = `${Cypress.env("apiUrl")}/activities?state=published`;
-  beforeEach(() => {});
+  beforeEach(() => {
+    const AUTH_URL = "/auth/sign-up";
+    const API_AUTH_URL = `${Cypress.env("apiUrl")}/register`;
+    cy.fixture("new-user").then((NEW_USER) => {
+      cy.intercept("POST", API_AUTH_URL, {
+        statusCode: 201,
+        fixture: "token",
+      }).as("postRegister");
+      cy.visit(AUTH_URL);
+      cy.registerUI(NEW_USER.username, NEW_USER.email, NEW_USER.password);
+    });
+  });
   context("when visits the home page", () => {
     beforeEach(() => {
       cy.visit(PAGE_URL);
     });
-    it("should display user menu", () => {});
+    it("should display user menu", () => {
+      cy.get("#user-menu").should("be.visible");
+    });
     it("should send the token to the server", () => {});
   });
 });
@@ -23,7 +36,7 @@ describe("Given an already registered and logged user", () => {
  *   should be redirected to the register page
  */
 describe("Given a secured endpoint returning 401", () => {
-  const REGISTER_URL = "http://localhost:4200/auth/sign-up";
+  const REGISTER_URL = "/auth/sign-up";
   const PAGE_URL = "http://localhost:4200/activities/mines";
   const API_URL = `${Cypress.env("apiUrl")}/activities/?userId=`;
   beforeEach(() => {});
---
+++ cypress/support/utils.ts
@@ -0,0 +1,8 @@
+export const TOKEN = {
+  accessToken: "xxx.xxx.xxx",
+  user: {
+    id: 1,
+    name: "John Doe",
+    email: "john@doe.com",
+  },
+};
