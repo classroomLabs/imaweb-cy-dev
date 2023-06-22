074f32378bc4eb620b960b99aa461fe6f6e52819
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-22 11:46:13 +0200

    feat: more examples and exercises

--- cypress/e2e/3-feat-owner-activities/3_2-create-activity.cy.ts
+++ cypress/e2e/3-feat-owner-activities/3_2-create-activity.cy.ts
@@ -21,8 +21,36 @@ describe("Given a registered user", () => {
   });
 
   context("When fill the form with valid data", () => {
-    beforeEach(() => {});
-    it("Then should send a post request to the API", () => {});
-    it("Then should navigate to the activities list page", () => {});
+    const activity = {
+      title: "Test activity",
+      description: "Test description",
+      location: "Test location",
+    };
+    beforeEach(() => {
+      cy.fixture("activities").then((activitiesElement) => {
+        const activities = activitiesElement as unknown as any[];
+        cy.intercept("POST", API_URL, {
+          body: activities,
+        }).as("postActivity");
+      });
+      cy.get("input[name=title]").type(activity.title);
+      cy.get("textarea[name=description]").type(activity.description);
+      cy.get("input[name=location]").type(activity.location);
+      cy.get("button[type=submit]").click();
+    });
+    it("Then should send a post request to the API", () => {
+      cy.wait("@postActivity").then((interception) => {
+        const { request } = interception;
+        const payload = request.body;
+        expect(payload.title).to.equal(activity.title);
+        cy.log("Payload", payload);
+        expect(payload.id).to.exist;
+      });
+    });
+    it("Then should navigate to the activities list page", () => {
+      cy.url().should("include", "/activities");
+    });
   });
 });
+
+// To Do: Use Page Object pattern
